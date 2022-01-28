f0d399cc142b8be11d5a3d78a4bf55adf0afb79b
pauline rouger <paulinerouge@hotmail.com>
2022-01-28 17:30:42 +0800

    qualifications section added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -29,7 +29,7 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== ACCORDEON SKILLS ====================*/
 const skillsContent = document.getElementsByClassName('skills__content'),
   skillsHeader = document.querySelectorAll('.skills__header');
 
@@ -48,3 +48,24 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener('click', toggleSkills);
 });
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active');
+    });
+
+    target.classList.add('qualification__active');
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active');
+    })
+    tab.classList.add('qualification__active');
+  })
+})
