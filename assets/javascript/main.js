9f7ec251ab4ca6f2ae3041d0679f3839ad78e912
pauline rouger <paulinerouge@hotmail.com>
2022-02-01 19:59:24 +0800

    added portfolio section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -71,24 +71,38 @@ tabs.forEach(tab => {
 })
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services__modal'),
-  modalBtns = document.querySelectorAll('.services__button'),
-  modalCloses = document.querySelectorAll('.services__modal-close');
+// const modalViews = document.querySelectorAll('.services__modal'),
+//   modalBtns = document.querySelectorAll('.services__button'),
+//   modalCloses = document.querySelectorAll('.services__modal-close');
 
-let modal = function (modalClick) {
-  modalViews[modalClick].classList.add('active-modal');
-};
+// let modal = function (modalClick) {
+//   modalViews[modalClick].classList.add('active-modal');
+// };
 
-modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener('click', () => {
-    modal(i);
-  })
-});
+// modalBtns.forEach((modalBtn, i) => {
+//   modalBtn.addEventListener('click', () => {
+//     modal(i);
+//   })
+// });
 
-modalCloses.forEach(modalClose => {
-  modalClose.addEventListener('click', () => {
-    modalViews.forEach((modalView) => {
-      modalView.classList.remove('active-modal');
-    });
-  });
+// modalCloses.forEach(modalClose => {
+//   modalClose.addEventListener('click', () => {
+//     modalViews.forEach((modalView) => {
+//       modalView.classList.remove('active-modal');
+//     });
+//   });
+// });
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
 });
